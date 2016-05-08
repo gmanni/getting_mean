1b8709f82c583db0be872c96c9b8a7929b9b1c5a
gmanni <gmanni@pcopernico.it>
2016-05-09 00:53:12 +0200

    aggiunta una minima parte gestione mongoose

--- sviluppo/app_server/models/db.js
+++ sviluppo/app_server/models/db.js
@@ -1,1 +1,20 @@
 var mongoose = require( 'mongoose' );
+
+// var dbURI = 'mongodb://localhost:27017/Loc8r';
+var dbURI = 'mongodb://localhost:27017/inventario';
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
+
+
+require('./locations');
