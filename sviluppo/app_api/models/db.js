d3dc8bf443acd12eb042a7c8e9713fdc3ac98f11
giman <gmanni@pcopernico.it>
2016-05-10 00:52:04 +0200

    inseriti tutti i metodi nei controller come segnaposto

--- sviluppo/app_api/models/db.js
+++ sviluppo/app_api/models/db.js
@@ -4,14 +4,13 @@ var mongoose = require( 'mongoose' );
 var dbURI = 'mongodb://localhost:27017/inventario';
 mongoose.connect(dbURI);
 
+// CONNECTION EVENTS
 mongoose.connection.on('connected', function () {
   console.log('Mongoose connected to ' + dbURI);
 });
-
 mongoose.connection.on('error',function (err) {
   console.log('Mongoose connection error: ' + err);
 });
-
 mongoose.connection.on('disconnected', function () {
   console.log('Mongoose disconnected');
 });
