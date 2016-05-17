ae751eabbbcffb99e22eb37dc66849e68333785e
gmanni <gmanni@pcopernico.it>
2016-05-17 18:05:41 +0200

    primo giro con api (non funziona)

--- sviluppo/Lo8r/app_server/controllers/locations.js
+++ sviluppo/Lo8r/app_server/controllers/locations.js
@@ -3,7 +3,9 @@ var apiOptions = {
   server : "http://localhost:3000"
 };
 
-module.exports.homelist = function(req, res){
+var renderHomepage = function(req, res, responseBody){
+  console.log("renderHomepage");
+  console.log(req);
   res.render('locations-list', {
     title: 'Loc8r - find a place to work with wifi',
     pageHeader: {
@@ -35,6 +37,24 @@ module.exports.homelist = function(req, res){
         }
       ]
 
+  });
+};
+
+module.exports.homelist = function(req, res){
+  var requestOptions, path;
+  path = '/api/locations';
+  requestOptions = {
+    url : apiOptions.server + path,
+    method : "GET",
+    json : {},
+    qs : {
+      lng : -0.7992599,
+      lat : 51.378091,
+      maxDistance : 20
+    }
+  };
+  request(requestOptions, function(err, response, body) {
+    renderHomepage(req, res, body);
   });
 };
 
